b6ecc0f4bce3baa41a4576c455cc78595ca1db93
Liao Yang <64757479+layang1208@users.noreply.github.com>
2021-05-17 17:42:26 -0400

    update

--- custom.js
+++ custom.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $(".post-wrapper").slick({
         centerMode: true,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
+        autoplay: true,
+        autoplaySpeed: 2000,
         centerPadding: '60px',
         slidesToShow: 3,
         nextArrow: $(".next"),
